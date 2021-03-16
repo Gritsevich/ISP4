388344bf2154c0a76d37f589d75a1da202edb4f0
rina <gritsevich.rina@gmail.com>
2021-03-16 16:59:25 +0300

    add program

---
+++ L1.py
@@ -0,0 +1,21 @@
+# sum of two numbers
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def main():
+    try:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+    except ValueError:
+        logging.error("Bad input ")
+    else:
+        logging.info("sum = %s", a + b)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        logging.error("KeybordInputError")
